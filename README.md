47eac47f51c47a8cb16ba64ed195af6b92b172d5
hossien <94741775+hossien014@users.noreply.github.com>
2023-09-25 13:41:50 +0330

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# random-quran
+
+
+# random quran ayah - ایات تصادفی از قران
+یک صفحه ساده که با هر بارگذاری یک ایه  تصادفی از قران را نمایش می دهد .
+
+simple web page  show radnom ayah of holy quran using ` https://alquran.cloud/api ` api. 
+
+![tablet view](tablet-veiw.png "tablet veiw").
+![tablet view](tablet-view2.png "tablet veiw").
+![mobile-view](mobile-view.png "mobile-view").
+
+
+
---
+++ script.js
@@ -0,0 +1,66 @@
+
+onload = insertAyeh();
+
+function genrateRadndomAyehNumber()
+{
+      const totalAyehInQuran = 6236;
+      let random_number = Math.floor((Math.random() * totalAyehInQuran) + 1);
+      return random_number;
+}
+
+function GetAyeh()
+{
+      let url = "http://api.alquran.cloud/v1/ayah/" + genrateRadndomAyehNumber();
+
+      return new Promise
+            (
+                  (resolve, recject) =>
+                  {
+
+
+                        fetch(url).then
+                              (
+                                    response =>
+                                    {
+                                          if (!response.ok) { console.log("error"); }
+                                          return response.json();
+
+                                    }
+                              )
+                              .then
+                              (
+                                    data =>
+                                    {
+                                          ayeh = data['data'];
+                                          resolve(ayeh);
+                                    }
+                              )
+                              .catch
+                              (
+                                    error =>
+                                    {
+                                          console.error('There was a problem with the fetch operation:', error);
+                                          recject("error")
+
+                                    }
+                              )
+
+                  }
+            )
+
+}
+
+
+
+function insertAyeh()
+{
+      GetAyeh().then((data =>
+      {
+            const ayeh = document.querySelector('.ayeh');
+            const ayehInfo = document.querySelector('#aye-info');
+            ayeh.innerHTML = data['text'];
+            console.log(data['text'].length);
+            ayehInfo.innerHTML = data['surah']['name'] + "  ( " + data['numberInSurah'] + " )";
+
+      }))
+}
